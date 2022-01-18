6e8bef1d4e4188aece80b3610636b78132b4bd61
Anillc <i@anillc.cn>
2022-01-18 21:26:57 +0800

    add tayga

--- machines/04 lasvegas/default.nix
+++ machines/04 lasvegas/default.nix
@@ -36,5 +36,32 @@ rec {
                 ${pkgs.iproute2}/bin/ip route add 10.127.20.114/32 dev deploy proto 114 table 114
             '';
         };
+        systemd.services.tayga = let
+            conf = pkgs.writeText "tayga" ''
+                tun-device nat64
+                ipv4-addr 10.127.3.1
+                dynamic-pool 10.127.3.0/24
+                prefix 2a0e:b107:1171::/96
+            '';
+        in {
+            wantedBy = [ "multi-user.target" ];
+            after = [ "network.target" ];
+            script = ''
+                ${pkgs.tayga}/bin/tayga --mktun --config ${conf}
+                ${pkgs.iproute2}/bin/ip link set nat64 up
+                ${pkgs.iproute2}/bin/ip route del 10.127.3.0/24 table 114 || true
+                ${pkgs.iproute2}/bin/ip route del 2a0e:b107:1171::/96 || true
+                ${pkgs.iproute2}/bin/ip route add 10.127.3.0/24 dev nat64 proto 114 table 114
+                ${pkgs.iproute2}/bin/ip route add 2a0e:b107:1171::/96 dev nat64
+                ${pkgs.tayga}/bin/tayga --nodetach --config ${conf}
+            '';
+        };
+        # tayga
+        networking.firewall.extraCommands = ''
+            ${pkgs.iptables}/bin/iptables -A FORWARD -s 10.127.3.0/24 -d 172.22.167.96/27 -j ACCEPT
+            ${pkgs.iptables}/bin/iptables -A FORWARD -s 10.127.3.0/24 -d 192.168.2.0/24 -j ACCEPT
+            ${pkgs.iptables}/bin/iptables -A FORWARD -s 10.127.3.0/24 -j DROP
+            ${pkgs.iptables}/bin/iptables -A FORWARD -j ACCEPT
+        '';
     };
 }
