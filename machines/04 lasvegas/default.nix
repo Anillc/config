0cfcee0076e412779a1037d56d34e00e48437d78
Anillc <i@anillc.cn>
2022-01-19 18:38:17 +0800

    update firewall

--- machines/04 lasvegas/default.nix
+++ machines/04 lasvegas/default.nix
@@ -59,12 +59,13 @@ rec {
         # tayga
         networking.firewall.extraCommands = ''
             ${pkgs.iptables}/bin/iptables -A FORWARD -s 10.127.3.0/24 -d 172.22.167.96/27 -j ACCEPT
-            ${pkgs.iptables}/bin/iptables -A FORWARD -s 10.127.3.0/24 -d 192.168.2.0/24 -j ACCEPT
+            ${pkgs.iptables}/bin/iptables -A FORWARD -s 10.127.3.0/24 -d 10.0.2.0/24 -j ACCEPT
             ${pkgs.iptables}/bin/iptables -A FORWARD -s 10.127.3.0/24 -j DROP
             ${pkgs.iptables}/bin/iptables -A FORWARD -j ACCEPT
         '';
         networking.firewall.extraStopCommands = ''
             ${pkgs.iptables}/bin/iptables -D FORWARD -s 10.127.3.0/24 -d 172.22.167.96/27 -j ACCEPT || true
+            ${pkgs.iptables}/bin/iptables -D FORWARD -s 10.127.3.0/24 -d 10.0.2.0/24 -j ACCEPT      || true
             ${pkgs.iptables}/bin/iptables -D FORWARD -s 10.127.3.0/24 -j DROP                       || true
             ${pkgs.iptables}/bin/iptables -D FORWARD -j ACCEPT                                      || true
         '';
