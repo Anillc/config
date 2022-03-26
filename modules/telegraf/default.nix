0c207b2ca694b23fb0f3de8a65e15e7858b98b13
Anillc <i@anillc.cn>
2022-03-26 08:16:31 +0800

    recover modules

---
+++ modules/telegraf/default.nix
@@ -0,0 +1,25 @@
+{ pkgs, ...}: {
+    services.telegraf = {
+        enable = true;
+        extraConfig = {
+            inputs = {
+                cpu = {
+                    percpu = true;
+                    totalcpu = true;
+                    collect_cpu_time = false;
+                    report_active = false;
+                };
+                mem = {};
+                disk.ignore_fs = [ "tmpfs" "devtmpfs" "devfs" "iso9660" "overlay" "aufs" "squashfs" ];
+                diskio = {};
+                net = {};
+            };
+            outputs.influxdb_v2 = {
+                urls = ["http://10.11.0.1:8086"];
+                token = "d2r575_TnHszNBVnApmX9-wwSFvpf0CQlCT85x8XGSa8hAMia6sjm4DCDwFhY42_oJR0_ie-ju1CYn8iYHI0HQ==";
+                organization = "AnillcNetwork";
+                bucket = "servers";
+            };
+        };
+    };
+}
