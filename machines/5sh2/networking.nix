a8b5155ad0ba64a413858eb3b411756bdd936d0d
Anillc <i@anillc.cn>
2022-04-14 18:29:52 +0800

    add v6 routes for sh2

--- machines/5sh2/networking.nix
+++ machines/5sh2/networking.nix
@@ -13,13 +13,13 @@ in {
     firewall.enableSourceFilter = false;
     systemd.network.networks.default-network = {
         matchConfig.Name = "ens18";
-        address = [ "192.168.1.110/24" ];
+        address = [ "192.168.1.110/24" "fe80::115/64" ];
         gateway = [ "192.168.1.1" ];
         routes = [
             { routeConfig = { Destination = "10.0.0.0/16";     PreferredSource = config.meta.v4; Gateway = "192.168.1.1"; Table = 114; Protocol = 114; }; }
             { routeConfig = { Destination = "192.168.2.0/24";  PreferredSource = config.meta.v4; Gateway = "192.168.1.1"; Table = 114; Protocol = 114; }; }
             { routeConfig = { Destination = "192.168.22.0/24"; PreferredSource = config.meta.v4; Gateway = "192.168.1.1"; Table = 114; Protocol = 114; }; }
-            # { routeConfig = { Destination = "5050:2333::/96";  PreferredSource = config.meta.v4; Gateway = "192.168.1.1"; Table = 114; Protocol = 114; }; }
+            { routeConfig = { Destination = "5050:2333::/96";  PreferredSource = config.meta.v6; Gateway = "fe80::c274:2bff:feff:a618"; Table = 114; Protocol = 114; }; }
         ];
     };
 }
