91f3176a6de5021e606b3a7aa29772ce88adceb6
Anillc <i@anillc.cn>
2022-07-25 18:48:39 +0800

    update bot

--- machines/1sh/bot.nix
+++ machines/1sh/bot.nix
@@ -15,7 +15,7 @@ with lib;
     virtualisation.oci-containers = {
         backend = "podman";
         containers.bot = {
-            image = "docker.io/anillc/cllina:46467d8";
+            image = "docker.io/anillc/cllina:81515f8";
             volumes = [
                 "/run/mysqld/mysqld.sock:/run/mysqld/mysqld.sock"
                 "${config.sops.secrets.bot-secrets.path}:/root/cllina/secrets.dhall"
