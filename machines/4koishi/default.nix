c28aac17eec5ca9e3573ab688572609416a569e2
Anillc <i@anillc.cn>
2022-10-24 01:07:41 +0800

    add plugins

--- machines/4koishi/default.nix
+++ machines/4koishi/default.nix
@@ -4,13 +4,13 @@ rec {
         name = "koishi";
         wg-public-key = "++8g+U89u77H0EbWI81j20CKKeSp7eY847M30sI2XFg=";
         syncthingId = "44JIF3B-D3EIAAE-F36UOOA-PM5CMFP-X2VZNQ3-5ZPPMCI-CIQDINJ-J5TRVQL";
-        # enable = false;
     };
     configuration = { config, pkgs, lib, ... }: {
         inherit meta;
         imports = [
             ./hardware.nix
             ./networking.nix
+            ./discourse.nix
         ];
         sops = {
             defaultSopsFile = ./secrets.yaml;
@@ -26,38 +26,5 @@ rec {
             };
         };
         bgp.enable = true;
-
-        services.postgresql.package = pkgs.postgresql_13;
-        services.discourse = {
-            enable = true;
-            hostname = "forum.koishi.xyz";
-            admin = {
-                username = "Koishi";
-                fullName = "Koishi";
-                email = "admin@forum.koishi.chat";
-                passwordFile = config.sops.secrets.discourse-admin.path;
-            };
-            mail = {
-                notificationEmailAddress = "noreply@forum.koishi.chat";
-                outgoing = {
-                    serverAddress = "smtp.zeptomail.com";
-                    port = 587;
-                    username = "emailapikey";
-                    passwordFile = config.sops.secrets.discourse-mail.path;
-                    domain = "forum.koishi.chat";
-                };
-            };
-        };
-
-        security.acme.certs."forum.koishi.xyz" = {
-            server = "https://acme-v02.api.letsencrypt.org/directory";
-            email = "admin@forum.koishi.chat";
-        };
-        firewall.publicTCPPorts = [ 80 443 ];
-        services.nginx = {
-            enable = true;
-            recommendedProxySettings = true;
-            recommendedTlsSettings = true;
-        };
     };
 }
---
+++ machines/4koishi/discourse.nix
@@ -0,0 +1,69 @@
+{ config, pkgs, lib, ... }:
+
+with builtins;
+with lib;
+
+let
+    plugins = with config.services.discourse.package.plugins; [
+        discourse-solved discourse-spoiler-alert discourse-math discourse-checklist
+        discourse-canned-replies discourse-github discourse-saved-searches
+        discourse-yearly-review
+        # discourse-feature-voting discourse-cakeday discourse-follow discourse-footnote
+        # discourse-signatures discourse-reactions discourse-push-notifications
+        # discourse-tooltips discourse-graphviz discourse-automation discourse-bcc
+        # discourse-upvotes discourse-category-experts discourse-characters-required
+        # discourse-restricted-replies discourse-templates
+        (config.services.discourse.package.mkDiscoursePlugin {
+            name = "discourse-reactions";
+            src = pkgs.fetchFromGitHub {
+                owner = "discourse";
+                repo = "discourse-reactions";
+                rev = "8bdbc4af68ddaced74b66aaf4046236ce3ff713c";
+                sha256 = "sha256-L9R+YD6BiZeZ53hxJGo3LBKjPajM7CmGbFrnRzIFUCA=";
+            };
+        })
+        (config.services.discourse.package.mkDiscoursePlugin {
+            name = "discourse-upvotes";
+            src = pkgs.fetchFromGitHub {
+                owner = "discourse";
+                repo = "discourse-upvotes";
+                rev = "c3527defe2abb18907b58eec110b42b0bc911447";
+                sha256 = "sha256-n6xBMzYq4BVFxnYtIaN0jBwf6MUl31NaZR8N220gQYw=";
+            };
+        })
+    ];
+in {
+    services.postgresql.package = pkgs.postgresql_13;
+    services.discourse = {
+        inherit plugins;
+        enable = true;
+        hostname = "forum.koishi.xyz";
+        admin = {
+            username = "Koishi";
+            fullName = "Koishi";
+            email = "admin@forum.koishi.chat";
+            passwordFile = config.sops.secrets.discourse-admin.path;
+        };
+        mail = {
+            notificationEmailAddress = "noreply@forum.koishi.chat";
+            outgoing = {
+                serverAddress = "smtp.zeptomail.com";
+                port = 587;
+                username = "emailapikey";
+                passwordFile = config.sops.secrets.discourse-mail.path;
+                domain = "forum.koishi.chat";
+            };
+        };
+    };
+
+    security.acme.certs."forum.koishi.xyz" = {
+        server = "https://acme-v02.api.letsencrypt.org/directory";
+        email = "admin@forum.koishi.chat";
+    };
+    firewall.publicTCPPorts = [ 80 443 ];
+    services.nginx = {
+        enable = true;
+        recommendedProxySettings = true;
+        recommendedTlsSettings = true;
+    };
+}
