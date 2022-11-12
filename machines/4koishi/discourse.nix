12ffdc8b6743f3717292c089de38f6dc730d04cc
Anillc <i@anillc.cn>
2022-11-12 10:00:38 +0800

    update acme server

--- machines/4koishi/discourse.nix
+++ machines/4koishi/discourse.nix
@@ -32,6 +32,7 @@ let
             };
         })
     ];
+    mapList = list: value: listToAttrs (map (flip nameValuePair value) list);
 in {
     services.postgresql.package = pkgs.postgresql_13;
     services.discourse = {
@@ -57,7 +58,7 @@ in {
         };
     };
 
-    security.acme.certs."forum.koishi.xyz" = {
+    security.acme.certs = mapList [ "forum.koishi.xyz" "www.koishi.xyz" "koishi.xyz" ] {
         server = "https://acme-v02.api.letsencrypt.org/directory";
         email = "admin@forum.koishi.chat";
     };
@@ -70,12 +71,12 @@ in {
             "koishi.xyz" = {
                 enableACME = true;
                 forceSSL = true;
-                locations."/".return = "301 https://koishi.chat";
+                locations."/".return = "302 https://koishi.chat";
             };
             "www.koishi.xyz" = {
                 enableACME = true;
                 forceSSL = true;
-                locations."/".return = "301 https://koishi.chat";
+                locations."/".return = "302 https://koishi.chat";
             };
         };
     };
