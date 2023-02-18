dd3d7c7510f284a778271bebc344d7fc3ddfd166
Anillc <i@anillc.cn>
2023-02-18 22:38:12 +0800

    recover network

--- machines/8jx/networking.nix
+++ machines/8jx/networking.nix
@@ -31,20 +31,20 @@ in {
             address = [ "${config.meta.v4}/32" "${config.meta.v6}/128" "${config.meta.externalV6}/128" ];
         }) config.wgi);
     }];
-    # services.cron = {
-    #     enable = true;
-    #     systemCronJobs = [ "*/20 * * * * root ${connect}" ];
-    # };
-    # systemd.services.connect-to-school = {
-    #     enable = true;
-    #     after = [ "network-online.target" "systemd-networkd.service" ];
-    #     partOf = [ "systemd-networkd.service" ];
-    #     wantedBy = [ "multi-user.target" ];
-    #     serviceConfig = {
-    #         Type = "oneshot";
-    #         RemainAfterExit = true;
-    #         Restart = "on-failure";
-    #     };
-    #     script = "${connect}";
-    # };
+    services.cron = {
+        enable = true;
+        systemCronJobs = [ "*/20 * * * * root ${connect}" ];
+    };
+    systemd.services.connect-to-school = {
+        enable = true;
+        after = [ "network-online.target" "systemd-networkd.service" ];
+        partOf = [ "systemd-networkd.service" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+            Type = "oneshot";
+            RemainAfterExit = true;
+            Restart = "on-failure";
+        };
+        script = "${connect}";
+    };
 }
