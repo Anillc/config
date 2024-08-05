e3f4fc534bc1101bf2c4254645ee3d481dcb9bc8
Anillc <i@anillc.cn>
2024-08-05 23:36:12 +0800

    reenable bot

--- machines/1cola/bot.nix
+++ machines/1cola/bot.nix
@@ -4,7 +4,7 @@ with builtins;
 with lib;
 
 let
-    bot = inputs.koinix.packages.${pkgs.system}.buildKoishi {
+    bot = inputs.koinix.lib.${pkgs.system}.buildKoishi {
         host = "0.0.0.0";
         port = 8056;
         prefix = "-";
@@ -12,11 +12,10 @@ let
         exitCommand = true;
         selfUrl = "https://bot.a";
         plugins = {
-            "@chronocat/koishi-plugin-adapter" = {
-                endpoint = "http://10.11.1.6:16530";
+            adapter-satori = {
+                endpoint = "http://127.0.0.1:5500";
                 token = "\${{ env.CHRONOCAT_TOKEN }}";
             };
-            "@chronocat/koishi-plugin-assets-memory" = {};
             adapter-discord.token = "\${{ env.DISCORD_TOKEN }}";
             adapter-telegram = {
                 protocol = "polling";
@@ -84,51 +83,51 @@ in {
         };
     };
 
-    # virtualisation.oci-containers = {
-    #     backend = "podman";
-    #     containers.pma = {
-    #         image = "docker.io/library/phpmyadmin";
-    #         volumes = [
-    #             "/run/mysqld/mysqld.sock:/tmp/mysql.sock"
-    #         ];
-    #         environment = {
-    #             PMA_HOST = "localhost";
-    #             # PMA_ARBITRARY = "1";
-    #         };
-    #         ports = [ "8444:80" ];
-    #     };
-    # };
-    # services.mysql = {
-    #     enable = true;
-    #     package = pkgs.mariadb;
-    #     initialDatabases = [{
-    #         name = "bot";
-    #     }];
-    # };
-    # services.telegraf.extraConfig.mysql.servers = [ "$MYSQL_SERVER" ];
-    # systemd.services."container@bot".after = [ "mysql.service" ];
-    # containers.bot = {
-    #     autoStart = true;
-    #     bindMounts."/run/secrets" = {};
-    #     bindMounts."/run/mysqld/mysqld.sock" = {};
-    #     config = {
-    #         system.stateVersion = "22.05";
-    #         security.pki.certificates = mkForce config.security.pki.certificates;
-    #         documentation.enable = false;
-    #         networking.firewall.enable = false;
-    #         i18n.defaultLocale = "zh_CN.UTF-8";
-    #         fonts.packages = with pkgs; [
-    #             jetbrains-mono
-    #             source-han-sans
-    #         ];
-    #         systemd.services.bot = {
-    #             wantedBy = [ "multi-user.target" ];
-    #             serviceConfig.EnvironmentFile = config.sops.secrets.bot-secrets.path;
-    #             script = ''
-    #                 mkdir -p /var/lib/bot && cd /var/lib/bot
-    #                 ${bot}/bin/koishi
-    #             '';
-    #         };
-    #     };
-    # };
+    virtualisation.oci-containers = {
+        backend = "podman";
+        containers.pma = {
+            image = "docker.io/library/phpmyadmin";
+            volumes = [
+                "/run/mysqld/mysqld.sock:/tmp/mysql.sock"
+            ];
+            environment = {
+                PMA_HOST = "localhost";
+                # PMA_ARBITRARY = "1";
+            };
+            ports = [ "8444:80" ];
+        };
+    };
+    services.mysql = {
+        enable = true;
+        package = pkgs.mariadb;
+        initialDatabases = [{
+            name = "bot";
+        }];
+    };
+    services.telegraf.extraConfig.mysql.servers = [ "$MYSQL_SERVER" ];
+    systemd.services."container@bot".after = [ "mysql.service" ];
+    containers.bot = {
+        autoStart = true;
+        bindMounts."/run/secrets" = {};
+        bindMounts."/run/mysqld/mysqld.sock" = {};
+        config = {
+            system.stateVersion = "22.05";
+            security.pki.certificates = mkForce config.security.pki.certificates;
+            documentation.enable = false;
+            networking.firewall.enable = false;
+            i18n.defaultLocale = "zh_CN.UTF-8";
+            fonts.packages = with pkgs; [
+                jetbrains-mono
+                source-han-sans
+            ];
+            systemd.tmpfiles.rules = [ "d /var/lib/bot 0700 root root" ];
+            systemd.services.bot = {
+                wantedBy = [ "multi-user.target" ];
+                serviceConfig = {
+                    EnvironmentFile = config.sops.secrets.bot-secrets.path;
+                    ExecStart = "${bot}/bin/koishi";
+                };
+            };
+        };
+    };
 }
