980b4e9a317b39bd0d43b9899b191952fca720bf
Anillc <i@anillc.cn>
2025-02-04 21:52:46 +0800

    route sigma

--- machines/2product/networking.nix
+++ machines/2product/networking.nix
@@ -12,8 +12,21 @@ in {
         { inherit (koishi.meta) id name wg-public-key; peer = 11002; cost = 690;  }
         { inherit (lux.meta)    id name wg-public-key; peer = 11002; cost = 4000; }
     ];
+    cfg.firewall.enableSourceFilter = false;
     systemd.network.networks.default-network = {
         matchConfig.Name = "ens18";
         DHCP = "yes";
     };
+    systemd.network.networks.ens20 = {
+        matchConfig.Name = "ens20";
+        networkConfig.Address = [ config.cfg.meta.v4 "fe80::2/64" ];
+    };
+    services.bird2.config = ''
+        protocol static {
+            route 10.11.1.3/32 via fe80::1%ens20 { krt_prefsrc = ${config.cfg.meta.v4}; };
+            ipv4 {
+                table igp_v4;
+            };
+        }
+    '';
 }
